22a7c6d824e9520db3e8146c7f5ebf01dc4cd7e3
Aman <yo.akaman@gmail.com>
2024-10-07 01:57:03 +0530

    Health Updated

--- journalapp/src/main/java/com/exdigest/journalapp/controller/healthcheck.java
+++ journalapp/src/main/java/com/exdigest/journalapp/controller/healthcheck.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class healthcheck {
     @GetMapping("/health-check")
         public String healthCheck(){
-            return "Ok";
+            return "Every thing is OK ";
 
     }
 }
